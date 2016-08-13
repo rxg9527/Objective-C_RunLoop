b73a923c28a1111f32c8e7521d74a1316f51b196
Yuen-iMac <rxg9527@sina.cn>
2016-08-13 10:13:15 +0800

    0813 添加到NSRunLoopCommonModes下可以解决问题

--- RunLoopExample/01_RunLoop相关类/ViewController.m
+++ RunLoopExample/01_RunLoop相关类/ViewController.m
@@ -34,8 +34,19 @@
      第一个参数:定时器
      第二个参数:runloop的运行模式
      */
-    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSDefaultRunLoopMode];
-    [[NSRunLoop currentRunLoop] addTimer:timer forMode:UITrackingRunLoopMode]; // 同时添加到UITrackingRunLoopMode下时可以解决问题
+    
+    /**
+     *  解决方法1
+     */
+//    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSDefaultRunLoopMode];
+//    [[NSRunLoop currentRunLoop] addTimer:timer forMode:UITrackingRunLoopMode]; // 同时添加到UITrackingRunLoopMode下时可以解决问题
+    
+    /**
+     *  解决方法2
+      NSRunLoopCommonModes = NSDefaultRunLoopMode + UITrackingRunLoopMode
+     占用,标签,凡是添加到NSRunLoopCommonModes中的事件爱你都会被同时添加到打上commmon标签的运行模式上
+     */
+    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
 }
 
 - (void)run {
