5ae2c2a629596e11ae762ab0f4a1cbd73906234b
Yuen-iMac <rxg9527@sina.cn>
2016-08-13 09:49:48 +0800

    0813 获取子线程对应RunLoop

--- RunLoopExample/RunLoopExample/ViewController.m
+++ RunLoopExample/RunLoopExample/ViewController.m
@@ -21,7 +21,8 @@
     // 获取线程对应RunLoop
     [self obtainRunLoop];
     
-    
+    // 获取子线程对应RunLoop
+    [self obtainChildThreadRunLoop];
 }
 
 - (void)obtainRunLoop {
@@ -48,6 +49,24 @@
      *  mainRunLoop:    0x7f8d8b4050d0
      */
     NSLog(@"\nmainRunLoop:%p",mainRunLoop.getCFRunLoop);
+}
+
+
+- (void)obtainChildThreadRunLoop {
+    //Runloop和线程的关系
+    //一一对应,主线程的runloop已经创建,但是子线程的需要手动创建
+    [[[NSThread alloc]initWithTarget:self selector:@selector(run) object:nil] start];
+}
+
+//在runloop中有多个运行模式,但是runloop只能选择一种模式运行
+//mode里面至少要有一个timer或者是source
+-(void)run
+{
+    NSLog(@"%s", __func__);
+
+    //如何创建子线程对应的runLoop,currentRunLoop懒加载的
+    NSLog(@"%@", [NSRunLoop currentRunLoop]);
+    NSLog(@"run---%@", [NSThread currentThread]);
 }
 
 @end
