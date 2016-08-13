3d74476369e21a677b5b1d3b86fa87904fd4aad5
Yuen-iMac <rxg9527@sina.cn>
2016-08-13 10:07:27 +0800

    0813 补充注释

--- RunLoopExample/01_RunLoop相关类/ViewController.m
+++ RunLoopExample/01_RunLoop相关类/ViewController.m
@@ -29,6 +29,7 @@
     NSTimer *timer = [NSTimer timerWithTimeInterval:0.5 target:self selector:@selector(run) userInfo:nil repeats:YES];
 
     //2.添加定时器到runLoop中,指定runloop的运行模式为NSDefaultRunLoopMode
+    // 这种情况下，滑动textView时，RunLoop运行模式变为UITrackingRunLoopMode，定时器不再运行
     /*
      第一个参数:定时器
      第二个参数:runloop的运行模式
