486cce8d0229df18c78abe0fa177246a26b6b620
Yuen-iMac <rxg9527@sina.cn>
2016-08-13 09:45:50 +0800

    0813 梳理

--- RunLoopExample/RunLoopExample/ViewController.m
+++ RunLoopExample/RunLoopExample/ViewController.m
@@ -18,9 +18,16 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    // 获取线程对应RunLoop
+    [self obtainRunLoop];
+    
+    
+}
+
+- (void)obtainRunLoop {
     //1.获得主线程对应的runloop
     NSRunLoop *mainRunLoop = [NSRunLoop mainRunLoop];
-
+    
     //2.获得当前线程对应的runLoop
     NSRunLoop *currentRunLoop = [NSRunLoop currentRunLoop];
     
@@ -29,19 +36,18 @@
      *  currentRunLoop: 0x7f8d8b709840
      */
     NSLog(@"\nmainRunLoop:%p\ncurrentRunLoop:%p", mainRunLoop, currentRunLoop);
-
+    
     // Core Foundation
     /**
      *  mainRunLoop:    0x7f8d8b4050d0
      *  currentRunLoop: 0x7f8d8b4050d0
      */
     NSLog(@"\nmainRunLoop:%p\ncurrentRunLoop:%p", CFRunLoopGetMain(), CFRunLoopGetCurrent());
-
+    
     /**
      *  mainRunLoop:    0x7f8d8b4050d0
      */
     NSLog(@"\nmainRunLoop:%p",mainRunLoop.getCFRunLoop);
 }
-
 
 @end
