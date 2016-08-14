1ffc890731b07f9886554bc67fedbb82a4a13503
Yuen-iMac <rxg9527@sina.cn>
2016-08-14 15:42:35 +0800

    0814 03_source和Observer initial commit

---
+++ RunLoopExample/03_source和Observer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  03_source和Observer
+//
+//  Created by Vincent on 16/8/14.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RunLoopExample/03_source和Observer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  03_source和Observer
+//
+//  Created by Vincent on 16/8/14.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
